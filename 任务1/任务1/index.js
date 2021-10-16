002cd0d662278eb51cabe9d31519dfcb7f5e9cbb
tq447789814 <447789814@qq.com>
2021-10-16 15:17:24 +0800

    first commit

---
+++ 任务1/任务1/index.js
@@ -0,0 +1,11 @@
+const tq=document.querySelector("#tq");
+const tq1=document.querySelector(".tq1");
+tq.addEventListener('mouseover',function(){
+tq1.style.display="block";
+})
+tq.addEventListener('mouseout',function(){
+ tq1.style.display="none";
+ })
+   
+       
+        
